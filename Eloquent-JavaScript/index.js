a7b6c98e93cf825aabf13a4ba8a6aa412b38086f
hisham-mhammed-afifi <hisham.mohmmed@gmail.com>
2022-11-06 02:18:20 +0200

    first ch in eloquent javascript

---
+++ Eloquent-JavaScript/index.js
@@ -0,0 +1,42 @@
+//...................................
+//    Ch1 - Eloquent JavaScript
+//...................................
+
+// ## Numbers
+13, 13.5, 2.99e2
+
+// ## Strings
+`Hello`, "Hello", 'Hello'
+
+// ## Unary Operator
+typeof 4.5
+typeof "x"
+-2, -10
+
+// ## Boolean Values
+// **Comparison
+3 > 2 // true
+"A" > "Z" //false
+NaN == NaN // false
+
+// **Logical operators
+true && false // false
+true && true // true
+true || false // true
+false || false // false
+
+// ## Empty Values
+null, undefined
+
+// ## Automatic Type Conversion
+8 * null // 0
+"5" - 1 // 4
+"5" + 1 // 51
+"five" + 2 // NaN
+false == 0 // true
+null == undefined // true
+null == 0 // false
+
+// **Short-Circuiting
+null || "user" // user
+"Hesham" || "user" // Hesham
